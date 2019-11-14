6cfd7a379030a86f80b62acd4a636de185fb28e9
cccm5 <danieldavidson@aol.com>
2019-11-13 22:52:17 -0500

    Parse control flow

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,20 +1,28 @@
 export class Graph<T>{
-    private readonly from: Map<T, T[]> = new Map<T,T[]>();
-    private readonly to: Map<T, T[]> = new Map<T, T[]>();
+    private readonly from = new Map<string,T[]>();
+    private readonly to = new Map<string, T[]>();
     public hasNode(element: T): boolean{
-        return this.from.has(element);
+        return this.from.has("" + element);
     }
     public addNode(element: T): void{
         if(this.hasNode(element))
             return;
-        this.from.set(element, []);
-        this.to.set(element, []);
+        this.from.set("" + element, []);
+        this.to.set("" + element, []);
     }
     public addEdge(begin: T, end: T): void{
         this.addNode(begin);
         this.addNode(end);
         // addNode will initialize the arrays if they don't exist
-        this.from.get(begin)!.push(end);
-        this.to.get(end)!.push(begin);
+        this.from.get("" + begin)!.push(end);
+        this.to.get("" + end)!.push(begin);
+    }
+
+    public getFrom(element: T): T[] | undefined{
+        return this.from.get("" + element);
+    }
+
+    public toString(): string{
+        return JSON.stringify(this.from);
     }
 }
--- src/fungespace/FungeSpace.ts
+++ src/fungespace/FungeSpace.ts
@@ -17,7 +17,24 @@ enum FlowControl {
     Random = '?',
 }
 
-type Flow = {dir: Direction, x: number, y: number};
+function getEnumKeyByEnumValue(myEnum: any, enumValue: string) {
+    let keys = Object.keys(myEnum).filter(x => myEnum[x] == enumValue);
+    return keys.length > 0 ? keys[0] : null;
+}
+
+type Flow = {dir: Direction, x: number, y: number, cord: Cordinate};
+class Cordinate{
+    public readonly x: number;
+    public readonly y: number;
+    constructor(x: number, y: number){
+        this.x = x;
+        this.y = y;
+    }
+
+    public toString(): string{
+        return "" + this.x + "," + this.y;
+    }
+}
 export class Fungespace {
     public static readonly WIDTH = 30;
     public static readonly HEIGHT = 80;
@@ -26,6 +43,7 @@ export class Fungespace {
         this.backing = new Uint8Array(Fungespace.WIDTH*Fungespace.HEIGHT);
         let xIndex = 0;
         let yIndex = 0;
+        this.backing.fill(" ".charCodeAt(0));
         for(let c of space){
             if(c == '\n'){
                 xIndex = 0;
@@ -37,12 +55,17 @@ export class Fungespace {
         }
         // we're moved once by prior itteration in the direction
         // Always check current token first, than itterate
-        const stack: Flow[] = [{dir: Direction.Right, x: 0, y:0}];
-        const flowTokens = Object.keys(FlowControl);
-        while(stack) {
+        const stack: Flow[] = [{dir: Direction.Right, x: 0, y:0, cord: new Cordinate(-1,0)}];
+        const flow = new Graph<Cordinate>();
+        const visited: string[] = [];
+        while(stack.length != 0) {
             let top: Flow = stack.pop()!;
             let x = top.x;
             let y = top.y;
+            if(visited.indexOf(JSON.stringify(top)) != -1 ){
+                continue;
+            }
+            visited.push(JSON.stringify(top));
             let stringMode = false;
             let token: string;
             do{
@@ -52,23 +75,50 @@ export class Fungespace {
                     d = 2;
                 } else if(token == '"'){
                     stringMode = !stringMode;
+                } else if(getEnumKeyByEnumValue(FlowControl, token) != null && !stringMode){
+                    break;
                 }
                 switch(top.dir){
                     case Direction.Up:
-                        y-=d;
+                        y = (y-d)%Fungespace.HEIGHT;
                         break;
                     case Direction.Down:
-                        y+=d;
+                        y = (y+d)%Fungespace.HEIGHT;
                         break;
                     case Direction.Left:
-                        x-=d;
+                        x = (x-d)%Fungespace.WIDTH;
                         break;
                     case Direction.Right:
-                        x+=d;
+                        x = (x+d)%Fungespace.WIDTH;
                         break;
                 }
-            }while(stringMode || flowTokens.indexOf(token)==-1);
-
+            }while((stringMode || getEnumKeyByEnumValue(FlowControl, token) == null) && !((x==top.x) && (y==top.y)));
+            flow.addEdge(top.cord, new Cordinate(x, y));
+            if(token == FlowControl.Right || token == FlowControl.HorizontalIf || token==FlowControl.Random){
+                stack.push({dir: Direction.Right,
+                           x: (x+1)%Fungespace.WIDTH,
+                           y: y, 
+                           cord: new Cordinate(x,y)});
+                 
+            }
+            if(token == FlowControl.Left || token == FlowControl.HorizontalIf || token==FlowControl.Random){
+                stack.push({dir: Direction.Left,
+                           x: (x-1)%Fungespace.WIDTH,
+                           y: y,
+                           cord: new Cordinate(x,y)});
+            }
+            if(token == FlowControl.Down || token == FlowControl.VerticalIf || token==FlowControl.Random){
+                stack.push({dir: Direction.Down,
+                           x: x,
+                           y: (y+1)%Fungespace.HEIGHT, 
+                           cord: new Cordinate(x,y)});
+            }
+            if(token == FlowControl.Up || token == FlowControl.VerticalIf || token==FlowControl.Random){
+                stack.push({dir: Direction.Up,
+                           x: x,
+                           y: (y-1)%Fungespace.HEIGHT,
+                           cord: new Cordinate(x,y)});
+            }
         }
     }
     public get(x: number, y: number): number{
@@ -86,8 +136,17 @@ export class Fungespace {
     public repr(): string{
         //TODO: Make this split rows by newlines
         let out = "";
+        let i = 0;
+        let newline = false;
         for(let c of this.backing){
             out += String.fromCharCode(c);
+            newline = newline || String.fromCharCode(c) != ' ';
+            i+=1;
+            if(i >= Fungespace.WIDTH && newline){
+                i=0;
+                out += '\n';
+                newline = false;
+            }
         }
         return out;
 
